28702f80f7585617b6e409089cbdeca1969b3f84
kigue001 <92765464+kigue001@users.noreply.github.com>
2021-11-01 18:06:44 +0900

    211029 (#9)
    * 레이아웃1
    
    * 설정 중
    
    * 갱신
    
    * 화면 구성 변경
    
    * 마지를 위한 커밋
    
    * 사이드바 메뉴 버튼 완성

---
+++ vite-vue3/src/layouts/LayoutDefault.vue
@@ -0,0 +1,38 @@
+<template>
+  <MainMenu msg="SideBar" />
+  <div class="main-wrapper">
+    <MainHeader />
+    <main class="main">
+        <slot name="main"></slot>
+    </main>
+    <MainFooter />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import MainMenu from '@components/MainMenu.vue';
+  import MainHeader from '@components/MainHeader.vue';
+  import MainFooter from '@components/MainFooter.vue';
+
+  import { onMounted } from 'vue';
+
+  onMounted(() => {
+    // 이벤트 핸들러 - 메뉴 카테고리 자동 펼침
+    document.querySelector('.btn-sidebar-toggle[data-toggle="collapse"]')?.addEventListener('click', (el: any) => {
+        console.log(el.value);
+    });
+    document.querySelectorAll('[data-toggle="collapse"]').forEach((e: Element) => {
+      if (e) {
+        let target: any = e.getAttribute('data-target');
+        if (target) {
+
+        }
+      }
+    });
+
+  });
+</script>
+
+<style scoped lang="scss">
+
+</style>
